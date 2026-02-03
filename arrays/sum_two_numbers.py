39fec72c254bef641e1d6e775ab51a5d38a30e7c
Tanvi_Badwaik <badwaiktanvi53@gmail.com>
2026-02-03 22:46:57 +0530

    Create sum_two_numbers.py

---
+++ arrays/sum_two_numbers.py
@@ -0,0 +1,15 @@
+# Problem: Sum of Two Numbers
+# Approach: Simple addition
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+
+def add(a, b):
+    return a + b
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+result = add(a, b)
+
+print("Sum =", result)
